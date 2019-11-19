12ae6f6e07de0f13d024276f561c7df67b9f9cc4
Mango0000 <51085780+Mango0000@users.noreply.github.com>
2019-11-19 14:46:10 +0100

    config fix

--- src/main/java/controller/BookShopServlet.java
+++ src/main/java/controller/BookShopServlet.java
@@ -5,13 +5,18 @@
  */
 package controller;
 
+import database.DB_Access;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import pojos.Book;
 
 /**
  *
@@ -29,8 +34,24 @@ public class BookShopServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    
+    
+    @Override
+    public void init()
+            throws ServletException {
+        super.init(); //To change body of generated methods, choose Tools | Templates.
+        DB_Access dba = new DB_Access();
+        try {
+            List<Book> b = dba.getAllBooksFromAuthor();
+            for (Book book : b) {
+                System.out.println(b.toString());
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(DB_Access.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
