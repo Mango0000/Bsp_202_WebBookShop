edfc3c25329c6abc940623eac110ef6da01ee643
Mango0000 <51085780+Mango0000@users.noreply.github.com>
2019-11-19 14:29:34 +0100

    config fix

--- src/main/java/database/DB_Config.java
+++ src/main/java/database/DB_Config.java
@@ -13,6 +13,6 @@ public interface DB_Config {
     public static final String DB_NAME = "BooksDB";
     public static final String DB_USER = "postgres";
     public static final String DB_PASSWORD = "postgres";
-    public static final String DB_URL = "jdbc:postgresql://localhost:5433/";
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/";
     public static final String DB_DRIVER = "org.postgresql.Driver";
 }
